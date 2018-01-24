7eee0ab75c6566f54af2c9cf2f858353bc75012e
xiaoy <yu.xiao@seaboxdata.com>
2018-01-24 15:11:43 +0800

    Finished the first version.

--- file-convert.pl
+++ file-convert.pl
@@ -3,17 +3,22 @@ use strict;
 use warnings FATAL => 'all';
 use Encode;
 
-open(my $fh, "d:\\data\\test1.txt") || die "failed to open input file.\n";
-open(my $fh_out, ">d:\\data\\test2.txt") || die "failed to open output file\n";
+sub file_convert {
+    my ($input_file, $output_file) = @_;
 
-while(<$fh>){
-    my $line = encode("utf8", decode("gbk", $_));
-    $line =~ s/[ ]+$//;
-    $line =~ s/[ ]+/|/g;
-    print $fh_out $line;
+    open(my $fh, $input_file) || die "failed to open input file.\n";
+    open(my $fh_out, ">$output_file") || die "failed to open output file\n";
+
+    while (<$fh>) {
+        my $line = encode("utf8", decode("gbk", $_));
+        $line =~ s/[ ]+$//;
+        $line =~ s/[ ]+/|/g;
+        print $fh_out $line;
+    }
+
+    close($fh);
+    close($fh_out);
+
 }
 
-close($fh);
-close($fh_out);
-
-
+file_convert('d:\data\test11.txt', 'd:\data\test2.txt');
